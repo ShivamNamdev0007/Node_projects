dc2df4673fe69444b8ab4dc36a00ef651f79482b
shivam01 <shivamnamdev32@gmail.com>
2024-11-23 23:08:46 +0530

    add assignment 3

---
+++ assign3/index.js
@@ -0,0 +1,56 @@
+
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) {
+        return "Error: Division by zero";
+    }
+    return a / b;
+}
+
+function reverse(str) {
+    return str.split("").reverse().join("");
+}
+ function isPalindrome(str) {
+    const cleanStr = str.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+    return cleanStr === cleanStr.split("").reverse().join("");
+}
+function removeDuplicates(str) {
+    return [...new Set(str)].join("");
+}
+ function removeNo(str) {
+    return str.replace(/[0123456789]/g, "");
+}
+   module.export{
+    add,
+    subtract,
+    multiply,
+    divide,
+    reverse,
+    isPalindrome,
+    removeDuplicates,
+    removeNo
+   }
+
+
+
+
+
+
+
+
+
+
+
+
