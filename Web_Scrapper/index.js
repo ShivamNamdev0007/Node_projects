93e3e804c9262156359a3601db806aa825c3b62b
shivam01 <shivamnamdev32@gmail.com>
2024-11-25 23:27:29 +0530

    update proj

--- Web_Scrapper/index.js
+++ Web_Scrapper/index.js
@@ -1,18 +1,19 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const XLSX = require('xlsx');
 
 async function scrapeWebsite(url) {
   try {
     const { data } = await axios.get(url);
     const $ = cheerio.load(data);
     
-    // Example: Extracting all the titles from a blog page
+    
     
     $('.sc-1hp8d8a-0 .sc-Ehqfj .bxOQva').each((index, element) => {
       console.log($(element).text());
     });
 
-    // console.log(titles);
+    console.log(titles);
   } catch (error) {
     console.error('Error fetching the webpage:', error);
   }
